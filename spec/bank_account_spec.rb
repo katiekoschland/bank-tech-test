e82adabe2d8f46951c79172505d7aff12d40d7e8
Katie Koschland <katiekoschland@hotmail.com>
2017-04-18 15:39:28 +0100

    Spec that Bank account can be instantiated

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,13 @@
 require 'bank_account'
 
 describe BankAccount do
-  subject(:bank_account) {BankAccount.new(0)}
-  it 'Creates a new bank account with a balance of zero' do
+  let(:bank_account) {BankAccount.new(0)}
+
+  it 'can be instantiated' do
+    expect(bank_account).to be_instance_of BankAccount
+  end
+
+  it 'creates a new bank account with a starting balance of zero' do
     expect(bank_account.balance).to eq(0)
   end
 end
