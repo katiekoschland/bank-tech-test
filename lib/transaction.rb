723b6460146a938950c974bf2c9efa9dbdca2dc6
Katie Koschland <katiekoschland@hotmail.com>
2017-04-18 18:35:42 +0100

    Type of transaction added to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,15 @@
 class Transaction
 
-  attr_reader :amount, :date
+  attr_reader :amount, :date, :transaction_type
 
   def initialize(amount, date = Time.new)
     @amount = amount
     @date = date
+    type_of_transaction
+  end
+
+  def type_of_transaction
+    @amount > 0 ? @transaction_type = :credit : @transaction_type = :debit
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,16 +1,29 @@
 require 'transaction'
 
 describe Transaction do
-  let(:transaction){Transaction.new(20, "10/01/2012")}
+  let(:transaction){ Transaction.new(1000, "10/01/2012") }
 
   describe 'initialize' do
-    it 'stores the amount of the transaction' do
-      expect(transaction.amount).to eq(20)
+    it 'the amount of the transaction' do
+      expect(transaction.amount).to eq(1000)
     end
 
-    it 'stores the date of the transaction' do
+    it 'the date of the transaction' do
       expect(transaction.date).to eq("10/01/2012")
     end
   end
-
 end
+
+  describe '#credit' do
+    let(:transaction){ Transaction.new(2000, "13/01/2012") }
+    it 'a positive amount will be defined as a credit ' do
+      expect(transaction.transaction_type).to be(:credit)
+    end
+  end
+
+  describe '#debit' do
+    let(:transaction){ Transaction.new(-500, "14-01-2012") }
+    it 'a negative amount will be defined as a debit ' do
+      expect(transaction.transaction_type).to be(:debit)
+    end
+  end
