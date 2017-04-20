d2d1c39dd3a1057157b6107216c8ba45a71078a0
Katie Koschland <katiekoschland@hotmail.com>
2017-04-20 14:05:56 +0100

    Transaction history stores amount and balance of transaction

--- spec/transaction_list_spec.rb
+++ spec/transaction_list_spec.rb
@@ -6,6 +6,11 @@ describe TransactionList do
   let(:transaction_class) {double(:transaction_class, new: transaction)}
   let(:transaction_list) {TransactionList.new(transaction_class)}
 
+  before do
+    allow(transaction).to receive(:amount){50}
+    allow(transaction).to receive(:balance){1000}
+  end
+
   describe '#initialize' do
     it 'initializes with an empty array' do
       expect(transaction_list.transaction_history).to be_empty
@@ -13,7 +18,18 @@ describe TransactionList do
   end
 
   describe '#transactions' do
-    it 'stores a transaction' do
+
+    it 'stores a transactions amount' do
+      transaction_list.create(50, 1000)
+      expect(transaction_list.transaction_history[0].amount).to eq(50)
+    end
+
+    it 'stores the ccurrent balance' do
+      transaction_list.create(50, 1000)
+      expect(transaction_list.transaction_history[0].balance).to eq(1000)
+    end
+
+    it 'creates a transaction' do
       transaction_list.create(50, 1000)
       expect(transaction_list.transaction_history).to include(transaction)
     end
